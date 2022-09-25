c7d5d02c736f014d74d68ebb8120b7684c3454e3
Xepobopa <ismartsdn4477@gmail.com>
2022-09-25 16:25:47 +0300

    Connect database FormShop and WebAPI. Create simple GET request

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using WebAPI.Controllers.Connection;
+using Dapper.Contrib.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using System.Linq;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+            var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
+
+            _products = connection.GetAll<Product>() as List<Product>;
+        }
+
+        [HttpGet("{Id}")]
+        public Product Get(int Id)
+        {
+            if (_products.Exists(x => x.Id.Equals(Id)) == false) return null;
+
+            return _products.FirstOrDefault(x => x.Id.Equals(Id));
+        }
+
+        public List<Product> _products { private set; get; }
+    }
+}
---
+++ WebAPI/Models/JsonModel.cs
@@ -0,0 +1,37 @@
+﻿namespace WebAPI.Models
+{
+    public class JsonModel
+    {
+        public JsonModel(string Source,
+            string Port,
+            string DatabaseName,
+            string UserId, string Password)
+        {
+            this.Source = Source;
+            this.Port = Port;
+            this.DatabaseName = DatabaseName;
+            this.UserId = UserId;
+            this.Password = Password;
+        }
+
+        public JsonModel()
+        {
+            this.Source = String.Empty;
+            this.Port = String.Empty;
+            this.DatabaseName = String.Empty;
+            this.UserId = String.Empty;
+            this.Password = String.Empty;
+        }
+
+        public string? Source { get; set; }
+        public string? Port { get; set; }
+        public string? DatabaseName { get; set; }
+        public string? UserId { get; set; }
+        public string? Password { get; set; }
+
+        public override string ToString()
+        {
+            return $"Source: {Source}, Port: {Port}, DatabaseName: {DatabaseName}, UserId: {UserId}, Password: {Password}";
+        }
+    }
+}
