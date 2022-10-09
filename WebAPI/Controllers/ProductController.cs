4e99f9ebf13e7a32d4b0e84d76149b119dd88581
Xepobopa <ismartsdn4477@gmail.com>
2022-10-09 13:54:01 +0300

    Add API CRUD architecture

--- WebAPI/Controllers/ProductController.cs
+++ WebAPI/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Dapper.Contrib.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Models;
 using System.Linq;
+using System.Net;
 
 namespace WebAPI.Controllers
 {
@@ -26,11 +27,43 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost("{Product}")]
-        public void Post(Product obj)
+        public void Add(Product obj)
         {
             var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
             _products.Add(obj);
             connection.Insert<Product>(obj);
+        }
+
+        [HttpDelete("{Id}")]
+        public HttpStatusCode Delete(int Id)
+        {
+            if (_products.Exists(x => x.Id.Equals(Id)) == false) return HttpStatusCode.NotFound;
+
+            var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
+            connection.Delete<Product>(_products.First(x => x.Id.Equals(Id)));
+            _products.Remove(_products.First(x => x.Id.Equals(Id)));
+
+            return HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Id - needed product. Other fields - what you need to change to
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        [HttpPost("{Id}/{newProduct}", Name = "Change")]
+        public HttpStatusCode ChangeProduct(int Id, Product newProduct)
+        {
+            Console.WriteLine($"{Id.GetType()} : {Id}");
+            Console.WriteLine($"{newProduct.GetType()} : {newProduct.Name}");
+            if (_products.Exists(x => x.Id.Equals(Id)) == false) 
+                return HttpStatusCode.NotFound;
+
+            Delete(Id);
+            Thread.Sleep(1000);
+            Add(newProduct);
+
+            return HttpStatusCode.OK;   
         }
 
         public List<Product> _products { private set; get; }
